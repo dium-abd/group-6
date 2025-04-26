7a42cf96c52d1ee093ff70bbb887129c3e6cc234
ricp6 <166334380+ricp6@users.noreply.github.com>
2025-04-26 23:25:24 +0100

    Q3 - 1m47s to 45s

--- analytical/Q3.sql
+++ analytical/Q3.sql
@@ -1,23 +1,25 @@
+-- Indexes used for better performance of the query
+CREATE INDEX idx_developer_name ON developer(name text_pattern_ops);
+CREATE INDEX idx_publisher_name ON publisher(name text_pattern_ops);
+
 -- arguments:
 --   name prefix (Ubisoft)
-SELECT count(*) AS total, count(DISTINCT id) AS unique
+EXPLAIN (ANALYZE, BUFFERS) SELECT count(*) AS total, count(DISTINCT id) AS unique
 FROM (
     (
-        SELECT p.name, u.id
+        SELECT p.name, l.user_id AS id
         FROM publisher p
         JOIN games_publishers gp ON gp.publisher_id = p.id
         JOIN game g ON g.id = gp.game_id
         JOIN library l ON l.game_id = g.id
-        JOIN users u ON u.id = l.user_id
     )
     UNION ALL
     (
-        SELECT d.name, u.id
+        SELECT d.name, l.user_id AS id
         FROM developer d
         JOIN games_developers gd ON gd.developer_id = d.id
         JOIN game g ON g.id = gd.game_id
         JOIN library l ON l.game_id = g.id
-        JOIN users u ON u.id = l.user_id 
     )
 )
 WHERE name LIKE 'Ubisoft%';
