f92d2c99a1dd16e0477615b6e9e0370111f16845
ricp6 <166334380+ricp6@users.noreply.github.com>
2025-04-29 17:02:05 +0100

    Q3 arg deixou de ser um pattern

--- analytical/Q3.sql
+++ analytical/Q3.sql
@@ -1,12 +1,5 @@
--- Indexes used for better performance of the query
-CREATE INDEX idx_developer_name ON developer(name text_pattern_ops);
-CREATE INDEX idx_publisher_name ON publisher(name text_pattern_ops);
-CREATE INDEX idx_games_publishers_pubid_gameid ON games_publishers (publisher_id, game_id);
-CREATE INDEX idx_games_developers_devid_gameid ON games_developers (developer_id, game_id);
-CREATE INDEX idx_library_game_user_including ON library (game_id, user_id) INCLUDE (user_id);
-
 -- correr cada vez que entrarmos no psql
-SET work_mem = '128MB'; -- Melhores valores: 128MB, 256MB
+SET work_mem = '128MB'; -- Testar melhores tempos
 
 -- arguments:
 --   name prefix (Ubisoft)
@@ -17,12 +10,12 @@ WHERE l.game_id IN (
     SELECT gp.game_id
     FROM publisher p
     JOIN games_publishers gp ON gp.publisher_id = p.id
-    WHERE p.name LIKE 'Ubisoft%'
+    WHERE p.name = 'Ubisoft'
     
     UNION ALL
     
     SELECT gd.game_id
     FROM developer d
     JOIN games_developers gd ON gd.developer_id = d.id
-    WHERE d.name LIKE 'Ubisoft%'
+    WHERE d.name = 'Ubisoft'
 );
--- analytical/auxQ3.sql
+++ analytical/auxQ3.sql
@@ -1,13 +1,14 @@
 -- Indexes used for better performance of the query
-CREATE INDEX idx_developer_name ON developer(name text_pattern_ops); -- text_pattern_ops para otimizar LIKE queries
-CREATE INDEX idx_publisher_name ON publisher(name text_pattern_ops);
+CREATE INDEX idx_developer_name ON developer(name);
+CREATE INDEX idx_publisher_name ON publisher(name);
 CREATE INDEX idx_games_publishers_pubid_gameid ON games_publishers (publisher_id, game_id);
 CREATE INDEX idx_games_developers_devid_gameid ON games_developers (developer_id, game_id);
 CREATE INDEX idx_library_game_user_including ON library (game_id, user_id) INCLUDE (user_id);
 
 -- tabela adicional para guardar o numero de users que comprou jogos de cada empresa
-CREATE TABLE company_users_count (
-    company_name VARCHAR PRIMARY KEY,
-    total_users INTEGER,
-    distinct_users INTEGER
-)
+--CREATE TABLE company_users_count (
+--    company_name VARCHAR PRIMARY KEY,
+--    total_users INTEGER,
+--    distinct_users INTEGER
+--)
+-- ver como usar isto (é preciso popular a tabela e atualizar a cada compra,etc)
