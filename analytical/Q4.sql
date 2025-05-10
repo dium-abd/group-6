c66e3d62f5805638bcff26b5d84e835562b7d09d
ricp6 <166334380+ricp6@users.noreply.github.com>
2025-05-10 23:37:52 +0100

    Q4 45s

--- analytical/Q4.sql
+++ analytical/Q4.sql
@@ -1,14 +1,23 @@
+-- This is important for the generate_series function to work correctly
+SET TIME ZONE 'UTC';
+
 -- arguments:
 --   lower date (2020-01-01)
 --   bin length (12 hours)
 EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS)
+WITH binned_library AS (
+  SELECT
+    date_bin('12 hours', added_date, '2020-01-01') AS bin,
+    buy_price
+  FROM library
+  WHERE added_date >= '2020-01-01'
+)
 SELECT
-    date_bin('12 hours', added_date, '2003-09-12') AS bin,
-    count(*) FILTER (WHERE buy_price > 0) AS paid_copies,
-    count(*) FILTER (WHERE buy_price = 0) AS free_copies,
-    sum(buy_price) AS money_generated
-FROM library
-WHERE added_date >= '2020-01-01'
+  bin,
+  count(*) FILTER (WHERE buy_price > 0) AS paid_copies,
+  count(*) FILTER (WHERE buy_price = 0) AS free_copies,
+  sum(buy_price) AS money_generated
+FROM binned_library
 GROUP BY bin
 ORDER BY bin;
 
